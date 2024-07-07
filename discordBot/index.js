6b7eea216415b3b80cedaa1d35b1c0fa3a2150bb
Dion Stopps <dionstopps@gmail.com>
2024-07-08 00:11:08 +0100

    IT FINALLY FUCKING WORKS HOLY SHIT

--- discordBot/index.js
+++ discordBot/index.js
@@ -35,6 +35,8 @@ const channel = client.channels.cache.find(channel => channel.name === channelNa
 
 //const user = client.users.cache.get(UserID)
 
+const excludedRoleNames = ['@everyone','@3rd UK Divison','@12th Armoured Infantry Bridgade','@Brigadier','@Colonel','@Major','@Captain','@Squadron Commander','@Squadron Executive','@Wing Commander','@Squadron Leader','@Lieutenant','@2nd. Lieutenant','@Warrent Officer 1','@Warrent Officer 2','@Staff Sergeant,','@Fight Lieutentant','@Sergeant','@Corporal','@Flying Officer','@[NCO]','@Lance Corporal','@Pilot Officer','@Infantry Specialist','@Armour Trooper','@Armour Cadet','@Officer Cadet','@Private','@[Enlisted]','@[Skr] Trainee','@Reservist','@Community Member','@Headquarters Company','@{{3rd Rifles Infantry Battalion}}','@{Black Templars Platoon}','@{Place Holder Testing}','@Castellan','@Sword Brethren','@Apothecary','@Initiate','@Neophyte','@Aspirant','@{20th Hussars Armour Squadron}','@{20th Reserve Armoured Squadron}','@Armour School','@230 Aviation Squadron','@Flight School','@Training Company','@Training Platoon Alpha (UK/EU)','@Training Platoon Bravo (NA/WP)','@Auxiliary Company','@{Reserve Infantry Company}','@Event Organizer','@BLUEFOR','@REDFOR','@Red Devils','@Iron Wolves','@Golden Eagles','@Legionaries','@Ravens','@Regular','@OHM Clan [Ω]','@Raidr Clan [RAIDR]','@[6thAB]','@[MAD]','@Lost Legion Clan [LL]','@[CH_SQ]','@[HOG]','@[EBOO]','@[TGM]','@Squad Community','@Community Member','@Operation Skira','@Administration Team','@Recruitment Team','@Events Team','@Looking for Group','@Galactic Contention','@Middle Eastern Escalation','@Ready or Not','@Hell Let Loose','@Ground Branch','@Hearts of Iron 4','@Caliber','@Squad 44','@Helldivers','@Civilization VI','@Project Zomboid','@Stellaris','@Chivalry 2','@Project Reality','@Config Manager','@!ADMIN','@Server Senior Admin','@Server Admin','@Server Moderator','@Server Booster','@L.O.A.',"@Nyk's Boys",'@N.F.F.C.','@GMT','@EU','@EST/CST','@PST/MST','@WP','@Asia/Africa','@mark'];
+
 client.on('messageCreate', (message) => {
   if (message.content.startsWith('!userinfo')) {
     const mention = message.mentions.users.first();
@@ -43,21 +45,27 @@ client.on('messageCreate', (message) => {
       const user = client.users.cache.get(UserID); // get the user themself 
       const guild = message.guild; // Get the guild from the message
       const member = guild.members.cache.get(UserID); // Get the member from the guild
+      const nickName = member.nickname; // Get the nickname of the member
       
-      const roles = member.roles.cache.filter(role => role.name!== '@everyone').map(role => role.name).join(', '); // Filter out @everyone role
+      const roles = member.roles.cache.filter(role =>!excludedRoleNames.includes(`@${role.name}`) && role.name!== '@everyone').sort((a, b) => b.position - a.position).map(role => role.name).join(', ');
 
       const embed = new EmbedBuilder()
         .setColor(0x0099FF)
         .setTitle('User Profile')
         .setAuthor({ name: 'Skira Information Bot' })
         .addFields(
-          { name: 'User Info:', value: user.tag, inline: false },
-          { name: 'User ID:', value: user.id, inline: false },
-          { name: 'Discord Join Date:', value: user.createdAt.toLocaleString(), inline: false },
-          { name: 'Server Join Date:', value: guild.members.cache.get(user.id).joinedAt.toLocaleString(), inline: false }, // Use guild here
+          { name: 'Discord Username:', value: user.tag, inline: true },
+          { name: 'Server Username:', value: nickName, inline: true},
+          { name: "\t", value: "\t" },
+          { name: 'User ID:', value: user.id, inline: true },
+          //{ name: "\t", value: "\t" },
+          { name: 'Discord Join Date:', value: user.createdAt.toLocaleString(), inline: true },
+          { name: "\t", value: "\t" },
+          { name: 'Server Join Date:', value: guild.members.cache.get(user.id).joinedAt.toLocaleString(), inline: true }, // Use guild here
+          { name: "\t", value: "\t" },
           { name: 'Roles:', value: roles, inline: true }, // Display the roles
-
-        );
+        )
+        .setFooter({ text: 'If there is any issues, dm "geek_x".' });
       message.channel.send({ embeds: [embed] });
     } else {
       message.reply('Please mention a user!');
